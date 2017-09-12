110c4153c9f715c11bdb896eea01cd57473c6aa4
ciara hudson <cchudson33@gmail.com>
2017-09-12 18:14:19 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,24 @@
+
+
+function shout(string) {
+}
+
+function shout(string) {
+console.log string.toUpperCase()
+}
+
+function whisper(string) {
+}
+
+function whisper(string) {
+  console.log string.toLowerCase()
+}
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string)
+  { return "I can\'t hear you!" }
+  if (string.toUpperCase() === string)
+  { return "YES INDEED!" }
+  if (string === "I love you, Grandma.")
+  { return "I love you, too." }
+}
