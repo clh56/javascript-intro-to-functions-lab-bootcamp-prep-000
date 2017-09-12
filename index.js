f5000e81fce8099da0d8fed112c634684de8429f
ciara hudson <cchudson33@gmail.com>
2017-09-12 18:37:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 function shout(string) {
 return string.toUpperCase()
+};
+
+shout("hello");
+
+function logshout(string) {
+  return console.log(string.toLowerCase())
 }
 
-function whisper(string) {
-  return string.toLowerCase()
+logShout ("hello");
+
+function whisper(string){
+  return string.toLowerCase();
 }
+
+whisper("HELLO");
+
+function logWhisper(string){
+  return console.log(string.toLowerCase())
+}
+
+logWhisper("HELLO")
+
 
 function sayHiToGrandma(string) {
   if (string.toLowerCase() === string)
